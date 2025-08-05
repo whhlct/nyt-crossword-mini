164ead4623902fd19a5c19c780f8cf80307b2358
whhlct <whhlct22@gmail.com>
2025-08-05 15:27:11 -0400

    Evaluated necessary headers, removed unecessary ones

--- main.py
+++ main.py
@@ -1,10 +1,17 @@
 import requests
 import json
-
+import time
 
 URL_PAGE = "https://www.nytimes.com/crosswords/game/mini/2025/08/03"
 URL_JSON = "https://www.nytimes.com/svc/crosswords/v6/puzzle/mini/2025-08-03.json"
 
+
+HEADERS = {
+    'accept': '*/*',
+    'accept-language': 'en-US,en;q=0.9',
+    'content-type': 'application/x-www-form-urlencoded',
+    'x-games-auth-bypass': 'true',
+}
 
 def get_page_cookies(url: str) -> dict[str, str]:
     """Fetch the NYT Mini Crossword page."""
@@ -16,35 +23,18 @@ def get_page_cookies(url: str) -> dict[str, str]:
     return response.cookies.get_dict()
 
 
-def get_mini(url: str, cookies: dict[str, str]) -> dict:
+def get_mini(url: str, cookies: dict[str, str], headers: dict[str, str] | None = None) -> dict:
     """Fetch the NYT Mini Crossword."""
 
-    headers = {
-        'accept': '*/*',
-        'accept-language': 'en-US,en;q=0.9',
-        'content-type': 'application/x-www-form-urlencoded',
-        'priority': 'u=1, i',
-        'sec-ch-ua': '"Not)A;Brand";v="8", "Chromium";v="138", "Google Chrome";v="138"',
-        'sec-ch-ua-mobile': '?0',
-        'sec-ch-ua-platform': '"Windows"',
-        'sec-fetch-dest': 'empty',
-        'sec-fetch-mode': 'cors',
-        'sec-fetch-site': 'same-origin',
-        'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/138.0.0.0 Safari/537.36',
-        'x-games-auth-bypass': 'true',
-    }
+    if headers is None:
+        headers = HEADERS
 
     response = requests.get(
         url, 
         headers=headers,
         cookies=cookies,
     )
-    if response.status_code == 200:
-        print("Successfully retrieved the crossword!")
-    else:
-        print("Failed to retrieve the crossword.")
-
-    print(f"Status code: {response.status_code}")
+    response.raise_for_status()  # Raise an error for bad responses
 
     # Return the JSON response if needed
     try:
@@ -53,6 +43,23 @@ def get_mini(url: str, cookies: dict[str, str]) -> dict:
     except ValueError:
         raise Exception("Failed to parse JSON response")
 
+
+def evaluate_unecessary_request_headers(url: str, cookies: dict[str, str]) -> None:
+    """Evaluate which request headers are unecessary.
+    Loops through each item in HEADERS, and tries a request without it."""
+    for header in HEADERS:
+        modified_headers = HEADERS.copy()
+        del modified_headers[header]
+
+        response = requests.get(
+            url,
+            headers=modified_headers,
+            cookies=cookies,
+        )
+        
+        print(f"{header:<30} - {response.status_code}")
+        
+        time.sleep(0.5)
 
 if __name__ == "__main__":
     # Fetch the NYT Mini Crossword page to get cookies
@@ -63,10 +70,15 @@ if __name__ == "__main__":
     for cookie_name, cookie_value in page_cookies.items():
         print(f"{cookie_name}: {cookie_value}")
     """
-
     # Fetch the NYT Mini Crossword using the cookies
     mini_data = get_mini(URL_JSON, page_cookies)
+    print(f"Mini Crossword Data:\n{json.dumps(mini_data)}")
+
+    # Evaluate unnecessary request headers
+    #evaluate_unecessary_request_headers(URL_JSON, page_cookies)
+    """
 
     # Save the crossword data to a file
     with open("mini_crossword_2.json", "w") as file:
         json.dump(mini_data, file, indent=4)
+    """
